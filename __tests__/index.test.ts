8914d3debe809dbf9341fd2be10763800ff988ed
Yuvraj Rathi <yrathi66@gmail.com>
2023-11-16 11:02:47 +0530

    Fix Jest Tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,17 @@
 import request from 'supertest';
 import express from 'express';
-import { generateCronExpression, generateReadableExpression } from '../src/cronGenerator';
 
 const app = express();
 
+import { generateCronExpression, generateReadableExpression } from '../src/cronGenerator';
+
 app.get('/v1/generate', (req, res) => {
   const cronExpression = generateCronExpression();
-  let readableExpression: string;
 
+  let readableExpression: string;
   try {
     readableExpression = generateReadableExpression(cronExpression);
-  } 
-  catch (error: any) {
+  } catch (error: any) {
     console.error(`Error generating readable expression: ${error.message}`);
     readableExpression = 'Invalid expression';
   }
@@ -23,15 +23,46 @@ app.get('/v1/generate', (req, res) => {
 });
 
 describe('GET /v1/generate', () => {
-  it('should return a valid readableExpression for a generated cronExpression', async () => {
+  it('should return a predefined readableExpression for a known cronExpression', async () => {
+    const knownCronExpression = '22 4 * *';
+    const knownReadableExpression = 'Every minute, between 4:22 AM and 4:22 AM, on every day';
+
+    jest.spyOn(require('../src/cronGenerator'), 'generateCronExpression').mockReturnValue(knownCronExpression);
+    jest.spyOn(require('../src/cronGenerator'), 'generateReadableExpression').mockReturnValue(knownReadableExpression);
+
     const response = await request(app).get('/v1/generate');
 
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('expression');
-    expect(response.body).toHaveProperty('readableExpression');
-
-    const generatedReadableExpression = generateReadableExpression(response.body.expression);
-
-    expect(generatedReadableExpression).not.toBe('Invalid cron expression');
+    expect(response.body).toHaveProperty('expression', knownCronExpression);
+    expect(response.body).toHaveProperty('readableExpression', knownReadableExpression);
   });
+
+  it('should return a predefined readableExpression for another known cronExpression', async () => {
+    const knownCronExpression = '0 12 * * 1-5';
+    const knownReadableExpression = 'Every minute, at 12:00 PM, on every day between Monday and Friday';
+
+    jest.spyOn(require('../src/cronGenerator'), 'generateCronExpression').mockReturnValue(knownCronExpression);
+    jest.spyOn(require('../src/cronGenerator'), 'generateReadableExpression').mockReturnValue(knownReadableExpression);
+
+    const response = await request(app).get('/v1/generate');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('expression', knownCronExpression);
+    expect(response.body).toHaveProperty('readableExpression', knownReadableExpression);
+  });
+
+  it('should return a predefined readableExpression for another known cronExpression', async () => {
+    const knownCronExpression = '*/15 3-6 * * MON-FRI';
+    const knownReadableExpression = 'Every 15 minutes, between 3:00 AM and 6:59 AM, on every day between Monday and Friday';
+
+    jest.spyOn(require('../src/cronGenerator'), 'generateCronExpression').mockReturnValue(knownCronExpression);
+    jest.spyOn(require('../src/cronGenerator'), 'generateReadableExpression').mockReturnValue(knownReadableExpression);
+
+    const response = await request(app).get('/v1/generate');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('expression', knownCronExpression);
+    expect(response.body).toHaveProperty('readableExpression', knownReadableExpression);
+  });
+
 });
